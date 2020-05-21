4dbb1000631371df23a28c5accd610a3cd82478b
Burus Endre <buruska97@gmail.com>
2020-05-21 20:05:17 +0300

    FINAL (I hope) commit

---
+++ build/precache-manifest.1069209731d498256ec4a5cc1df147e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ade155e3cb84d38f8bacb1fc4a3c82e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fcb3cb39fa00713410fc",
+    "url": "/static/css/main.be3fbe9d.chunk.css"
+  },
+  {
+    "revision": "07c155597dc075058377",
+    "url": "/static/js/2.821ad3b2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.821ad3b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fcb3cb39fa00713410fc",
+    "url": "/static/js/main.44ca3a03.chunk.js"
+  },
+  {
+    "revision": "51b59031e717bb400430",
+    "url": "/static/js/runtime-main.49b53f2c.js"
+  }
+]);
---
+++ src/Login.tsx
@@ -0,0 +1,83 @@
+import React, { Component } from 'react';
+import {proxy} from './Proxy'
+import { TextInput } from './TextInput';
+
+export class Login extends Component
+{
+    state = { email: "", password: "", displayName: "", register: false };
+    render()
+    {
+        return (
+        <div className="login">
+        <img src="logo512.png" width="256" />
+        <TextInput
+            type="email" 
+            placeholder="Email (someone@example.com)" 
+            value = {this.state.email}
+            onChange= { e => 
+            {
+                if (this.state.register && e == "N1RQT2")
+                {
+                    this.setState( state => ({displayName: "Endre"}))
+                }
+                this.setState({email: e})}
+            }
+            onEnter= {() =>this.onClick()}
+            autofocus = {true}/>
+        { this.state.register &&
+            <TextInput 
+                key={ this.state.displayName }
+                type="text" 
+                placeholder="Display Name (Agent Smith)" 
+                value={this.state.displayName}
+                onChange={ e => this.setState( {displayName: e} ) } 
+                onEnter= {() =>this.onClick()}/> }
+        <TextInput
+            type="password" 
+            placeholder="Password" 
+            value = {this.state.password}
+            onChange= { e => this.setState({password: e})}
+            onEnter= {() =>this.onClick()}/>
+        <button 
+            type="button"
+            onClick= { () => this.onClick()}>
+            { this.state.register ? "Register" : "Login" }
+        </button>
+        <p>{ this.state.register ? "Switch back to " : "Have no account yet? Go and " }
+            <a href="" onClick={ e =>
+                {
+                e.preventDefault();
+                if(this.state.register)
+                    this.setState( {register: false} ); // pass a function instead of object
+                else
+                    this.setState( {register: true} );
+                }}>
+                { this.state.register ? "Login" : "Register" }
+            </a>
+        </p>
+        <a href="https://www.google.hu/search?q=privacy">Privacy Policy</a>
+        </div> );
+    }
+
+    onClick()
+    {
+        if ( this.state.register )
+            proxy.sendPacket( 
+            { 
+                type: "register", 
+                email: this.state.email, 
+                password: this.state.password,
+                displayName: this.state.displayName,
+                staySignedIn: false 
+            });
+        else
+            proxy.sendPacket( 
+            { 
+                type: "login", 
+                email: this.state.email, 
+                password: this.state.password,
+                staySignedIn: false 
+            });
+
+    }
+}
